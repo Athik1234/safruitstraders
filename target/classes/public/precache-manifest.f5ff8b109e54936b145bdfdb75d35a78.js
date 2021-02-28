65ae5587ddcbfc5ef8c1bdacdf9ade67f9be1319
BlessonDaniel <61042237+BlessonDaniel@users.noreply.github.com>
2021-02-28 17:56:41 +0530

    Add files via upload

---
+++ target/classes/public/precache-manifest.f5ff8b109e54936b145bdfdb75d35a78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e86c2872fb2859e7b79778ad3c6751",
+    "url": "/index.html"
+  },
+  {
+    "revision": "520e7f8c909b94994f94",
+    "url": "/static/css/main.eb5a5787.chunk.css"
+  },
+  {
+    "revision": "b3d242f618149fddc424",
+    "url": "/static/js/2.d0ccb0fd.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.d0ccb0fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520e7f8c909b94994f94",
+    "url": "/static/js/main.7e884e4c.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);
